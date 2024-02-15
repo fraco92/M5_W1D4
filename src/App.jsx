42bf40dc788a9e53f2575de1bbbac2876d2728f2
fraco92 <fraco92@gmail.com>
2024-02-15 16:27:26 +0100

    filter added

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,13 @@ import { Navbar } from "./components/MyNav";
 import { Welcome } from "./components/Welcome";
 // import { getBooks } from "./api/api";
 import { AllTheBooks } from "./components/AllTheBooks";
-import { useState } from "react";
-import { useEffect } from "react";
-import scifi from "./assets/json_books/scifi.json";
 
 function App() {
   return (
     <>
       <Navbar links={["Home", "About", "Browse"]} />
       <Welcome />
-      <AllTheBooks books={scifi} />
+      <AllTheBooks />
       <Footer />
     </>
   );
--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -5,27 +5,38 @@
 
 import { useState } from "react";
 import { useEffect } from "react";
-import scifi from "./assets/json_books/scifi.json";
+import scifi from "../assets/json_books/scifi.json";
 
 import { SingleBook } from "./SingleBook.jsx";
 
 export const AllTheBooks = () => {
   // const allBooks = [...fantasy, ...history, ...horror, ...romance, ...scifi];
 
+  const [bookFilter, setBookFilter] = useState("");
+
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
-    AllTheBooks().then((books) => {
-      setBooks(scifi);
-    });
+    setBooks(books);
   }, []);
 
-  const showBooks = books.map((book, index) => (
-    // <div key={index} className="border">
-    <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 grid-flow-row gap-4 mx-8 mb-16">
-      <SingleBook key={index} book={book} />
-    </div>
-  ));
+  const bookList = () =>
+    scifi.filter((book) =>
+      book.title.toLowerCase().includes(bookFilter.toLowerCase())
+    );
 
-  return <>{showBooks}</>;
+  return (
+    <>
+      <input
+        onChange={(e) => setBookFilter(e.currentTarget.value)}
+        type="text"
+        placeholder="Cerca qui il tuo libro"
+      />
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 grid-flow-row gap-4 mx-8 mb-16">
+        {bookList().map((book, index) => (
+          <SingleBook key={index} book={book} />
+        ))}
+      </div>
+    </>
+  );
 };
