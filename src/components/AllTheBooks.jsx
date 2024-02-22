2295133216964d8237974c53fd344cb7c37eefce
fraco92 <fraco92@gmail.com>
2024-02-22 13:11:40 +0100

    Context created - all works fine

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,44 +1,15 @@
-// import fantasy from "../assets/json_books/fantasy.json";
-// import history from "../assets/json_books/history.json";
-// import horror from "../assets/json_books/horror.json";
-// import romance from "../assets/json_books/romance.json";
-
-import { useState } from "react";
-import { useEffect } from "react";
-// import scifi from "../assets/json_books/scifi.json";
-import { getBooks } from "../api/api.js";
-
 import { SingleBook } from "./SingleBook.jsx";
+import { useContext } from "react";
+import { BookContext } from "../context/BookContext.jsx";
 
 export const AllTheBooks = () => {
-  // const allBooks = [...fantasy, ...history, ...horror, ...romance, ...scifi];
-
-  const [bookFilter, setBookFilter] = useState("");
-
-  const [books, setBooks] = useState([]);
-
-  useEffect(() => {
-    getBooks(books).then((books) => {
-      setBooks(books);
-    });
-  }, []);
-
-  const bookList = () =>
-    books.filter((book) =>
-      book.title.toLowerCase().includes(bookFilter.toLowerCase())
-    );
+  const { filterBooks } = useContext(BookContext);
 
   return (
     <>
       <div className="grid">
-        <input
-          className="mb-4 mx-auto rounded-md text-center border border-slate-300 focus:outline-none focus:border-slate-600"
-          onChange={(e) => setBookFilter(e.currentTarget.value)}
-          type="text"
-          placeholder="Cerca qui il tuo libro"
-        />
         <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 grid-flow-row gap-4 mx-8 mb-16">
-          {bookList().map((book, index) => (
+          {filterBooks.map((book, index) => (
             <SingleBook key={index} book={book} />
           ))}
         </div>
--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -1,18 +1,41 @@
+import { useContext } from "react";
+import { BookContext } from "../context/BookContext";
+
 export const Navbar = ({ links }) => {
+  const { books, setFilterBooks } = useContext(BookContext);
+
+  const handlerInput = (e) => {
+    const filter = e.currentTarget.value;
+    setFilterBooks(bookList(filter));
+  };
+
+  const bookList = (filter) =>
+    books.filter((book) =>
+      book.title.toLowerCase().includes(filter.toLowerCase())
+    );
+
   return (
     <div className="bg-neutral-100 py-3 shadow-xl shadow-gray-300/50 flex justify-center gap-1">
-      <img
-        className="h-[32px]"
-        src="./public/books-study-learning-education-reading-library-svgrepo-com.svg"
-        alt=""
+      <div>
+        <img
+          className="h-[32px]"
+          src="./public/books-study-learning-education-reading-library-svgrepo-com.svg"
+          alt=""
+        />
+        <ul className="flex justify-center gap-5">
+          {links.map((link, index) => (
+            <li key={index}>
+              <a href={"#" + link}>{link}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <input
+        className="mb-4 mx-auto rounded-md text-center border border-slate-300 focus:outline-none focus:border-slate-600"
+        onChange={handlerInput}
+        type="text"
+        placeholder="Cerca qui il tuo libro"
       />
-      <ul className="flex justify-center gap-5">
-        {links.map((link, index) => (
-          <li key={index}>
-            <a href={"#" + link}>{link}</a>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 };
