6e6df384f03128c6fe4e882bf34439d063a643f6
fraco92 <fraco92@gmail.com>
2024-03-02 15:46:17 +0100

    page "NotFound" 404 added

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import NotFoundImg from "../assets/404_error_img.jpg";
+
+export const NotFound = () => {
+  return (
+    <div>
+      <img src={NotFoundImg} alt="notFound_img" />
+    </div>
+  );
+};
